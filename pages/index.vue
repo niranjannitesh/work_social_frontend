8117d5018118a35208ad5274191276d58840215c
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2019-09-02 00:52:22 +0530

    Added Invite User

--- pages/index.vue
+++ pages/index.vue
@@ -158,6 +158,42 @@
       </app-tab>
       <app-tab title="Users">
         <div class="py-4 px-4">
+          <div class="flex justify-end">
+            <app-button type="outlined">
+              <svg
+                class="mr-2 fill-current"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  opacity="0.5"
+                  d="M15 16C12.31 16 9.23 17.28 9 18H21C20.8 17.29 17.7 16 15 16Z"
+                  fill-opacity="0.9"
+                />
+                <path
+                  opacity="0.5"
+                  d="M15 10C16.1046 10 17 9.10457 17 8C17 6.89543 16.1046 6 15 6C13.8954 6 13 6.89543 13 8C13 9.10457 13.8954 10 15 10Z"
+                  fill-opacity="0.9"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M19 8C19 10.21 17.21 12 15 12C12.79 12 11 10.21 11 8C11 5.78998 12.79 4 15 4C17.21 4 19 5.78998 19 8ZM17 8C17 6.90002 16.1001 6 15 6C13.8999 6 13 6.90002 13 8C13 9.09998 13.8999 10 15 10C16.1001 10 17 9.09998 17 8Z"
+                  fill-opacity="0.9"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M7 18C7 15.34 12.3301 14 15 14C17.6699 14 23 15.34 23 18V20H7V18ZM15 16C12.3101 16 9.21997 17.28 9 18H21C20.8 17.29 17.7 16 15 16Z"
+                  fill-opacity="0.9"
+                />
+                <path d="M6 15V12H9V10H6V7H4V10H1V12H4V15H6Z" fill-opacity="0.9" />
+              </svg>
+              Invite User
+            </app-button>
+          </div>
           <table class="text-left w-full border-collapse table-users">
             <tr class="head">
               <th></th>
